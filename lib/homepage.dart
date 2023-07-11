78f2a89a7418797bcbd654ac7d62122ed5ed0cc4
jeeva <jeevajeeva060402@gmail.com>
2023-07-11 12:05:51 +0530

    initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,91 @@
+import 'package:clocify_clone/profile.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBarScreen extends StatefulWidget {
+  const BottomNavBarScreen({super.key});
+
+  @override
+  _BottomNavBarScreenState createState() => _BottomNavBarScreenState();
+}
+
+class _BottomNavBarScreenState extends State<BottomNavBarScreen> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Clockify Clone',
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: const Icon(Icons.more_time, color: Colors.black),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.person,
+              color: Colors.black,
+            ),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ListViewScreen(),
+                ),
+              );
+            },
+          ),
+        ],
+        backgroundColor: Colors.blue[300],
+      ),
+      body: Center(
+        child: Text(
+          'Page ${_currentIndex + 1}',
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue[300],
+            icon: const Icon(
+              Icons.access_time,
+              color: Colors.black,
+            ),
+            label: 'Time tracker',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue[300],
+            icon: const Icon(
+              Icons.calendar_month,
+              color: Colors.black,
+            ),
+            label: 'Calender',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue[300],
+            icon: const Icon(
+              Icons.bar_chart,
+              color: Colors.black,
+            ),
+            label: 'Reports',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.blue[300],
+            icon: const Icon(
+              Icons.folder,
+              color: Colors.black,
+            ),
+            label: 'Projects',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/profile.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ListViewScreen extends StatefulWidget {
+  const ListViewScreen({super.key});
+
+  @override
+  State<ListViewScreen> createState() => _ListViewScreenState();
+}
+
+class _ListViewScreenState extends State<ListViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile Info'),
+      ),
+    );
+  }
+}
